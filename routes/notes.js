f4eb4c99d72d342030393a044ffd6e67fbdc8cd3
Matthew Davis <matthewdavis1372@yahoo.com>
2022-07-06 02:07:31 -0400

    cleaning up variables in routes

--- routes/notes.js
+++ routes/notes.js
@@ -14,18 +14,23 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     console.info(`${req.method} request received, updating current data.`);
-    req.body.id = uuidv4();
+
+    const id = uuidv4();
     const newNote = req.body;
-    if (req.body.title && req.body.text && req.body.id) {
+
+    if (newNote.title && newNote.text) {
         fs.readFile(notesFilePath, (err, data) => {
             if (err) {
                 console.log(err);
                 return res.status(400).json(err);
             }
+
             try {
                 const parsedData = JSON.parse(data);
                 parsedData.push(newNote);
+
                 fs.writeFileSync(notesFilePath, JSON.stringify(parsedData));
+
                 res.status(201);
                 res.end();
             } catch (err) {
@@ -34,29 +39,27 @@ router.post('/', (req, res) => {
             }
         });
     }
-    }
-)
+});
 
 router.delete('/:id', (req, res) =>{
     let deleteNoteId = req.params.id;
+    
     fs.readFile(notesFilePath, (err, data) => {
         try {
             const parsedData = JSON.parse(data);
-            // Is there a data structure that you could use, that prevents you 
-            // from needing to loop through an array to find if an id exists?
-            for (let i=0; i<parsedData.length; i++)
-            {
-                
+            for (let i=0; i<parsedData.length; i++) {
                 if (parsedData[i].id === deleteNoteId){
                     parsedData.splice(i-1,1);
-                    fs.writeFileSync(notesFilePath, JSON.stringify(parsedData));
+                    fs.writeFileSync(notesFilePath, JSON.stringify(parsedData));                  
                 }
             }
+            res.status(204);
+            res.end();
         } catch (err) {
             res.status(400).json(err);
         }
 
-        });
-    })
+    });
+})
 
 module.exports = router;
